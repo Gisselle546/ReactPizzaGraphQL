64ba03af21b6bb860e7a5ee7f3369bcf7aaa4f4b
Gisselle Rodriguez <gissellerodriguez431@gmail.com>
2021-01-04 11:51:58 -0500

    update cart dialog

--- client/src/Components/dialog.js
+++ client/src/Components/dialog.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Dialog,DialogActions,DialogContent,DialogTitle,Button } from '@material-ui/core';
-
+import {withRouter} from 'react-router-dom';
 
 function CustomDialog(props) {
     
@@ -10,8 +10,8 @@ function CustomDialog(props) {
 return(
     <Dialog open={props.open} close={props.onCloseModal}>
           <DialogTitle>{props.title}</DialogTitle>
-          <DialogContent > <img height="30%" width="30%" src={props.content}/> </DialogContent>
-          <DialogActions> <Button onClick={props.onCloseModal} color="primary">Close</Button><Button>Go To Cart</Button></DialogActions>
+          <DialogContent > <img height="350px" width="350px" src={props.content}/> </DialogContent>
+          <DialogActions> <Button onClick={props.onCloseModal} color="primary">Close</Button><Button onClick={props.history.push('/cart')}>Go To Cart</Button></DialogActions>
 
     </Dialog>
 
@@ -21,4 +21,4 @@ return(
 
 }
 
-export default CustomDialog;
+export default withRouter(CustomDialog);
