c9a33bd02af9cf8c445edf23a32caa1d813e7dd6
Gisselle Rodriguez <gissellerodriguez431@gmail.com>
2021-01-04 11:46:18 -0500

    update cart dialog

--- client/src/Components/dialog.js
+++ client/src/Components/dialog.js
@@ -10,8 +10,8 @@ function CustomDialog(props) {
 return(
     <Dialog open={props.open} close={props.onCloseModal}>
           <DialogTitle>{props.title}</DialogTitle>
-          <DialogContent > <img height="400px" width="400px" src={props.content}/> </DialogContent>
-          <DialogActions> <Button onClick={props.onCloseModal} color="primary">Close</Button></DialogActions>
+          <DialogContent > <img height="30%" width="30%" src={props.content}/> </DialogContent>
+          <DialogActions> <Button onClick={props.onCloseModal} color="primary">Close</Button><Button>Go To Cart</Button></DialogActions>
 
     </Dialog>
 
